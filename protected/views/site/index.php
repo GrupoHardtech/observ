56f953004d24edde64ed4e64570c23cbd1708e34
smithhorna <smithhorna@gmail.com>
2013-10-22 19:06:39 -0500

    Products

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -3,19 +3,23 @@
   <div class="block features">
     <h2 class="title-divider"><span>OBSERV <span class="de-em">Products</span></span> <small>Core features included in all products.</small></h2>
     <div class="row">
-      <div class="feature col-sm-6 col-md-3"> <img src="img/features/HT-55PIXIM.png" alt="Feature 1" class="img-responsive" />
+      <div class="feature col-sm-6 products-list"> <img src="img/features/HT-55PIXIM.png" alt="Feature 1" class="img-responsive" />
         <h3 class="title">Mobile <span class="de-em">Friendly</span></h3>
         <p>Rhoncus adipiscing, magna integer cursus augue eros lacus porttitor magna. Dictumst, odio! Elementum tortor sociis in eu dis dictumst pulvinar lorem nec aliquam a nascetur.</p>
       </div>
-      <div class="feature col-sm-6 col-md-3"> <img src="img/features/HT-IP02SN.png" alt="Feature 2" class="img-responsive" />
+      <div class="feature col-sm-6 products-list"> <img src="img/features/DVR.png" alt="Feature 2" class="img-responsive" />
         <h3 class="title">24/7 <span class="de-em">Support</span></h3>
         <p>Rhoncus adipiscing, magna integer cursus augue eros lacus porttitor magna. Dictumst, odio! Elementum tortor sociis in eu dis dictumst pulvinar lorem nec aliquam a nascetur.</p>
       </div>
-      <div class="feature col-sm-6 col-md-3"> <img src="img/features/feature-3.png" alt="Feature 3" class="img-responsive" />
+      <div class="feature col-sm-6 products-list"> <img src="img/features/HVR.png" alt="Feature 3" class="img-responsive" />
         <h3 class="title">Free Upgrade <span class="de-em">Assistance</span></h3>
         <p>Rhoncus adipiscing, magna integer cursus augue eros lacus porttitor magna. Dictumst, odio! Elementum tortor sociis in eu dis dictumst pulvinar lorem nec aliquam a nascetur.</p>
       </div>
-      <div class="feature col-sm-6 col-md-3"> <img src="img/features/feature-4.png" alt="Feature 4" class="img-responsive" />
+      <div class="feature col-sm-6 products-list"> <img src="img/features/NVR.png" alt="Feature 4" class="img-responsive" />
+        <h3 class="title">99.9% <span class="de-em">Uptime</span></h3>
+        <p>Rhoncus adipiscing, magna integer cursus augue eros lacus porttitor magna. Dictumst, odio! Elementum tortor sociis in eu dis dictumst pulvinar lorem nec aliquam a nascetur.</p>
+      </div>
+      <div class="feature col-sm-6 products-list"> <img src="img/features/HT-KB1010.png" alt="Feature 4" class="img-responsive" />
         <h3 class="title">99.9% <span class="de-em">Uptime</span></h3>
         <p>Rhoncus adipiscing, magna integer cursus augue eros lacus porttitor magna. Dictumst, odio! Elementum tortor sociis in eu dis dictumst pulvinar lorem nec aliquam a nascetur.</p>
       </div>
--- protected/views/site/products.php
+++ protected/views/site/products.php
@@ -9,8 +9,8 @@
     <li><a href="#" data-quicksand-fid="type-media">Accessories</a></li>
   </ul>
   
-  <!--Customers-->
   <ul class="row list-unstyled block customers">
+    <!--CAMERA-->
     <li class="customer type-design" data-quicksand-id="id-0" data-quicksand-tid="type-design"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-3012S.jpg" alt="Google"></span><br>
     <span class="title">DOME CAMERA OBSERV HT-3012S</span> <span class="description">OBSERV DOMO CAMERA HT-3012S / 1/3 SONY / 420 TVL / 24 PCS / 15M DISTANCE / 3.6 MM LENS / COLOR WHITE / PLASTIC HOUSING</span> </span> </a> </li>
     <li class="customer type-design" data-quicksand-id="id-1" data-quicksand-tid="type-design"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-B5515.jpg" alt="CSS3"></span><br>
@@ -35,17 +35,62 @@
     <span class="title">WIFI IP CAMERA HT - 1607</span> <span class="description">WIFI IP CAMERA HT - 1607 / SENSOR 1/4 COMS / CAMERA LENS 3.6 MM / VIDEO STANDARD VGA: 640 * 480 PIXELS / MINIILUMINACION 0.5 LUX / NIGHT VISION OF 5 MS / PORT RJ 45.</span> </span> </a> </li>
     <li class="customer type-web" data-quicksand-id="id-11" data-quicksand-tid="type-web"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-126PIXIM.png" alt="YouTube"></span><br>
     <span class="title">PIXIM CAMERA HT-126PIXIM</span> <span class="description">HT-126PIXIM PIXIM CAMERA LENS 1/3 "DPS SENSOR (PIXIM) WDR 690 TVL, 3.6MM FIXED LENS, LED EA 20 20 METERS / VANDAL / BLACK COLOR.</span> </span> </a> </li>
-    <li class="customer type-media" data-quicksand-id="id-9" data-quicksand-tid="type-media"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-401C.png" alt="Vodafone"></span><br>
+    <li class="customer type-media" data-quicksand-id="id-12" data-quicksand-tid="type-media"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-401C.png" alt="Vodafone"></span><br>
     <span class="title">PIR HIDDEN CAMERA HT-401C OBSERV</span> <span class="description">0.8 LUX 420 TVL / 1/3 "SONY CCD CAMERA 3.7MM Pinhole PIR / WITH WALL BRACKET</span> </span> </a> </li>
-    <li class="customer type-media" data-quicksand-id="id-10" data-quicksand-tid="type-media"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-340J.jpg" alt="BitBucket"></span><br>
+    <li class="customer type-media" data-quicksand-id="id-13" data-quicksand-tid="type-media"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-340J.jpg" alt="BitBucket"></span><br>
     <span class="title">MIRROR HIDDEN CAMERA HT-340J OBSERV</span> <span class="description">0.8 LUX 420 TVL / 1/3 "SONY CCD CAMERA 3.7MM Pinhole PIR / WITH WALL BRACKET</span> </span> </a> </li>
-    <li class="customer type-web" data-quicksand-id="id-11" data-quicksand-tid="type-web"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-380C.jpg" alt="YouTube"></span><br>
+    <li class="customer type-web" data-quicksand-id="id-14" data-quicksand-tid="type-web"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-380C.jpg" alt="YouTube"></span><br>
     <span class="title">HIDDEN CAMERA SENSOR SMOKE OBSERV HT-380C</span> <span class="description">0.8 LUX  420 TVL 1/3"SONY CCD 3.7MM PINHOLE CAMERA PIR/WITH WALL BRACKET</span> </span> </a> </li>
-    <li class="customer type-media" data-quicksand-id="id-9" data-quicksand-tid="type-media"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-LC001.jpg" alt="Vodafone"></span><br>
+    <li class="customer type-media" data-quicksand-id="id-15" data-quicksand-tid="type-media"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-LC001.jpg" alt="Vodafone"></span><br>
     <span class="title">LIGHTER SPY CAMERA 720P TYPE HT-LC001</span> <span class="description"></span> </span> </a> </li>
-    <li class="customer type-media" data-quicksand-id="id-10" data-quicksand-tid="type-media"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-010.jpg" alt="BitBucket"></span><br>
+    <li class="customer type-media" data-quicksand-id="id-16" data-quicksand-tid="type-media"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-010.jpg" alt="BitBucket"></span><br>
     <span class="title">CAP SPY CAMERA</span> <span class="description"></span> </span> </a> </li>
-    <li class="customer type-web" data-quicksand-id="id-11" data-quicksand-tid="type-web"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-706.jpg" alt="YouTube"></span><br>
+    <li class="customer type-web" data-quicksand-id="id-17" data-quicksand-tid="type-web"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-706.jpg" alt="YouTube"></span><br>
     <span class="title">KEYRING HD SPY CAMERA</span> <span class="description"></span> </span> </a> </li>
+    <!-- END CAMERA-->
+
+    <!-- DVR -->
+    <li class="customer type-design" data-quicksand-id="id-0" data-quicksand-tid="type-design"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-8116.png" alt="Google"></span><br>
+    <span class="title">16-CHANNEL DVR HT-8116</span> <span class="description">"VIDEO INPUT: 16 CHANNEL COMPOSITE INPUT WITH 1.0VP-P 75O BNC VIDEO OUTPUT: 2 CHANNEL COMPOSITE INPUT WITH 1.0VP-P 75O BNC"/"AUDIO INPUT:4 CHANNEL 600O RCA AUDIO OUTPUT:1 CHANNEL 600O RCA"/SUPPORTS REALTIME CIF RECORDING/SUPPORTS 16 CH PLAYBACK SIMULTANEOUSLY</span> </span> </a> </li>
+    <li class="customer type-design" data-quicksand-id="id-1" data-quicksand-tid="type-design"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-6016.jpg" alt="CSS3"></span><br>
+    <span class="title">16-CHANNEL DVR OBSERV HT-6016</span> <span class="description">OBSERV 16 CHANNEL DVR HT-​​6016 / RECORDER H.264 480 FPS HIGH DEFINITION IMAGE, SUPPORTS 16CH CIF / 16 VIDEO IN / 1 VIDEO OUT / 1 VGA / 2 AUDIO IN / 1 AUDIO OUT / MAX 2TB 2HDD/MAX CMS SOFTWARE, INCLUDED MOUSE, REMOTE CONTROL.</span> </span> </a> </li>
+    <li class="customer type-web" data-quicksand-id="id-2" data-quicksand-tid="type-web"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-7016.jpg" alt="Yahoo"></span><br>
+    <span class="title">16-CHANNEL DVR OBSERV HT-7016</span> <span class="description">OBSERV 16 CHANNEL DVR HT-​​7016 / WRITER HIGH DEFINITION IMAGE H.264 480 FPS, SUPPORTS 16CH CIF / 16 VIDEO INPUT / 1 OUTPUT VIDEO / 1 VGA / 2 AUDIO / 4 ALARM INPUT / 1 OUTPUT ALARM / 1 AUDIO / MAX 2HDD / MAX 2TB CMS SOFTWARE INCLUDES MOUSE, REMOTE CONTROL.</span> </span> </a> </li>
+    <li class="customer type-design" data-quicksand-id="id-4" data-quicksand-tid="type-design"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-9624.jpg" alt="HTML5"></span><br>
+    <span class="title">24-CHANNEL DVR OBSER VHT-9624 </span> <span class="description">24 CHANNEL DVR OBSERV HT-9624 / WRITER HIGH DEFINITION IMAGE H.264 480 FPS FULL SUPPORT 24CH CIF / 24 VIDEO IN / 2 OUT VIDEO / 1 VGA / 4 IN AUDIO / 8 ALARM INPUT / 1 OUTPUT ALARM / 1 OUTPUT AUDIO / MAX 2HDD / MAX 4TB CMS SOFTWARE INCLUDES MOUSE, REMOTE CONTROL.</span> </span> </a> </li>
+    <li class="customer type-web" data-quicksand-id="id-3" data-quicksand-tid="type-web"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-9632.jpg" alt="LESS"></span><br>
+    <span class="title">32-CHANNEL DVR OBSERV HT-9632</span> <span class="description">32 CHANNEL DVR OBSERV HT-9632 / WRITER HIGH IMAGE DEFINIC H.264 480 FPS FULL SUPPORT 32CH CIF / 32 VIDEO INPUTS / OUTPUTS VIDEO 2/1 VGA / 4 AUDIO INPUTS / 8 ALARM INPUT / 1 OUTPUT ALARM / 1 AUDIO / MAX 2HDD / MAX 4TB CMS SOFTWARE INCLUDES MOUSE, REMOTE CONTROL.</span> </span> </a> </li>
+    <li class="customer type-web" data-quicksand-id="id-5" data-quicksand-tid="type-web"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-2314LV.jpg" alt="GitHub"></span><br>
+    <span class="title">4-CHANNEL DVR OBSERV HT- 2314LV</span> <span class="description">4 CHANNEL DVR OBSERV HT-2314LV / IMAGE RECORDER HIGH DEFINITION H.264 120 FPS, FULL SUPPORT 4CH CIF / DVR 4 VIDEO INPUTS / OUTPUTS VIDEO 1/1 VGA / 4 AUDIO INPUT / 1 OUTPUT AUDIO / MX 1HDD / MAX 2TB CMS SOFTWARE / P2P, INCLUDES MOUSE, REMOTE CONTROL.</span> </span> </a> </li>
+    <li class="customer type-design" data-quicksand-id="id-6" data-quicksand-tid="type-design"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-2004DALARMA.png" alt="eBay"></span><br>
+    <span class="title">4-CHANNEL ALARM OBSERV DVR HT-2004D </span> <span class="description">COMPRESSION FORMAT: H.264 TOTAL FRAMES: 100FPS/VIDEO INPUT: 4CH BNC OUTPUT: VGA / BNC, SUPPORTING/1CABLE FOR SATA HD 1500G, ALARM IN / OUT: 4CH/1CH / / SUPPORTING IR REMOTE-CONTROLLER, MOUSE CONTROL, USB / / BACKUP, NETWORK, CONTROL PTZ, OFFERING MULTI-LANGUAGEOPERAITION, SUPPORTING MOTION DETECTION, TIMING</span> </span> </a> </li>
+    <li class="customer type-web" data-quicksand-id="id-7" data-quicksand-tid="type-web"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-7318.jpg" alt="Amazon"></span><br>
+    <span class="title">2-CHANNEL DVR HT - 7318</span> <span class="description">2 CHANNEL DVR HT - 7318 / AUDIO 4 / AUDIO OUTPUT 1 / ALARM 8 INPUT / OUTPUT ALARM 1 / SPACE MAX 2 TB.</span> </span> </a> </li>
+    <li class="customer type-design" data-quicksand-id="id-8" data-quicksand-tid="type-design"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-1515A.jpg" alt="jQuery"></span><br>
+    <span class="title">PTZ IP CAMERA HT-1515A</span> <span class="description">PTZ IP CAMERA HT-1515A / SENSOR quarter / 3.6MM LENS CAMERA / VIDEO STANDARD: VGA 640 X 480 PIXELS / FPS: 25FPS / COMPRESSION MINI LIGHTING: 0.5LUX / NIGHT VISION INFRARED 5 MS / STANDARD PORT RJ45 10 / 100 / VISUAL EXPLORATION HORIZONTAL VERTICAL 120 270 / AL</span> </span> </a> </li>
+    <li class="customer type-media" data-quicksand-id="id-9" data-quicksand-tid="type-media"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-9612.jpg" alt="Vodafone"></span><br>
+    <span class="title">TUBULAR IP CAMERA HT-9612</span> <span class="description">IP CAMERA TUBE HT-9612/2 MEGAPIXEL / FIXED LENS 2.8-12MM / 42LED TO 40 METERS / SOFTWARE SUPPORT AND SOFTWARE MULTI CMS / SUPPORT PROTOCOL: TCP, UDP, IP, HTTP, FTP. NTP, IPSEC UPNP, RTP, RTCP ANDA WEATHERPROOF / WHITE</span> </span> </a> </li>
+    <li class="customer type-media" data-quicksand-id="id-10" data-quicksand-tid="type-media"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-1607.jpg" alt="BitBucket"></span><br>
+    <span class="title">WIFI IP CAMERA HT - 1607</span> <span class="description">WIFI IP CAMERA HT - 1607 / SENSOR 1/4 COMS / CAMERA LENS 3.6 MM / VIDEO STANDARD VGA: 640 * 480 PIXELS / MINIILUMINACION 0.5 LUX / NIGHT VISION OF 5 MS / PORT RJ 45.</span> </span> </a> </li>
+    <li class="customer type-web" data-quicksand-id="id-11" data-quicksand-tid="type-web"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-126PIXIM.png" alt="YouTube"></span><br>
+    <span class="title">PIXIM CAMERA HT-126PIXIM</span> <span class="description">HT-126PIXIM PIXIM CAMERA LENS 1/3 "DPS SENSOR (PIXIM) WDR 690 TVL, 3.6MM FIXED LENS, LED EA 20 20 METERS / VANDAL / BLACK COLOR.</span> </span> </a> </li>
+    <li class="customer type-media" data-quicksand-id="id-12" data-quicksand-tid="type-media"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-401C.png" alt="Vodafone"></span><br>
+    <span class="title">PIR HIDDEN CAMERA HT-401C OBSERV</span> <span class="description">0.8 LUX 420 TVL / 1/3 "SONY CCD CAMERA 3.7MM Pinhole PIR / WITH WALL BRACKET</span> </span> </a> </li>
+    <li class="customer type-media" data-quicksand-id="id-13" data-quicksand-tid="type-media"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-340J.jpg" alt="BitBucket"></span><br>
+    <span class="title">MIRROR HIDDEN CAMERA HT-340J OBSERV</span> <span class="description">0.8 LUX 420 TVL / 1/3 "SONY CCD CAMERA 3.7MM Pinhole PIR / WITH WALL BRACKET</span> </span> </a> </li>
+    <li class="customer type-web" data-quicksand-id="id-14" data-quicksand-tid="type-web"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-380C.jpg" alt="YouTube"></span><br>
+    <span class="title">HIDDEN CAMERA SENSOR SMOKE OBSERV HT-380C</span> <span class="description">0.8 LUX  420 TVL 1/3"SONY CCD 3.7MM PINHOLE CAMERA PIR/WITH WALL BRACKET</span> </span> </a> </li>
+    <li class="customer type-media" data-quicksand-id="id-15" data-quicksand-tid="type-media"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-LC001.jpg" alt="Vodafone"></span><br>
+    <span class="title">LIGHTER SPY CAMERA 720P TYPE HT-LC001</span> <span class="description"></span> </span> </a> </li>
+    <li class="customer type-media" data-quicksand-id="id-16" data-quicksand-tid="type-media"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-010.jpg" alt="BitBucket"></span><br>
+    <span class="title">CAP SPY CAMERA</span> <span class="description"></span> </span> </a> </li>
+    <li class="customer type-web" data-quicksand-id="id-17" data-quicksand-tid="type-web"> <a > <span class="inner-wrapper"> <span class="img-wrapper"><img src="img/customers/HT-706.jpg" alt="YouTube"></span><br>
+    <span class="title">KEYRING HD SPY CAMERA</span> <span class="description"></span> </span> </a> </li>
+    <!-- END DVR -->
+
+
   </ul>
+  
+
+
 </div>
