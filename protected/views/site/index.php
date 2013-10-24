48842864cbd13ab673576e5582e0edf5b137bda3
smithhorna <smithhorna@gmail.com>
2013-10-23 19:28:21 -0500

    Observ 99%

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -3,25 +3,42 @@
   <div class="block features">
     <h2 class="title-divider"><span>OBSERV <span class="de-em">Products</span></span> <small>Core features included in all products.</small></h2>
     <div class="row">
-      <div class="feature col-sm-6 products-list"> <img src="img/features/HT-55PIXIM.png" alt="Feature 1" class="img-responsive" />
-        <h3 class="title">Mobile <span class="de-em">Friendly</span></h3>
-        <p>Rhoncus adipiscing, magna integer cursus augue eros lacus porttitor magna. Dictumst, odio! Elementum tortor sociis in eu dis dictumst pulvinar lorem nec aliquam a nascetur.</p>
+      <div class="feature col-sm-6 products-list"> <img src="img/features/IPCamera.png" alt="Feature 1" class="img-responsive" />
+        <h3 class="title">IP CAMERA</h3>
+        <spam>Box Camera</spam><br>
+        <spam>Indoor Dome Camera</spam><br>
+        <spam>Outdoor Dome Camera</spam><br>
+        <spam>Minidome Camera</spam><br>
+        <spam>IR Bullet Camera</spam><br>
+        <spam>Cube Camera</spam><br>
+        <spam>PTZ Camera</spam>
       </div>
-      <div class="feature col-sm-6 products-list"> <img src="img/features/DVR.png" alt="Feature 2" class="img-responsive" />
-        <h3 class="title">24/7 <span class="de-em">Support</span></h3>
-        <p>Rhoncus adipiscing, magna integer cursus augue eros lacus porttitor magna. Dictumst, odio! Elementum tortor sociis in eu dis dictumst pulvinar lorem nec aliquam a nascetur.</p>
+      <div class="feature col-sm-6 products-list"> <img src="img/features/HT-55PIXIM.png" alt="Feature 2" class="img-responsive" />
+        <h3 class="title">PIXIM CAMERA</h3>
+        <spam>Pixim Dome Camera</spam><br>
+        <spam>Pixim Bullet Camera</spam>
       </div>
-      <div class="feature col-sm-6 products-list"> <img src="img/features/HVR.png" alt="Feature 3" class="img-responsive" />
-        <h3 class="title">Free Upgrade <span class="de-em">Assistance</span></h3>
-        <p>Rhoncus adipiscing, magna integer cursus augue eros lacus porttitor magna. Dictumst, odio! Elementum tortor sociis in eu dis dictumst pulvinar lorem nec aliquam a nascetur.</p>
+      <div class="feature col-sm-6 products-list"> <img src="img/features/PTZ.png" alt="Feature 3" class="img-responsive" />
+        <h3 class="title">ANALOG CAMERA</h3>
+        <spam>Box Camera</spam><br>
+        <spam>IR Dome Camera</spam><br>
+        <spam>Mini Camera</spam><br>
+        <spam>IR Bullet Camera</spam><br>
+        <spam>Hidden Camera</spam><BR>
+        <spam>PTZ Camera</spam>
       </div>
       <div class="feature col-sm-6 products-list"> <img src="img/features/NVR.png" alt="Feature 4" class="img-responsive" />
-        <h3 class="title">99.9% <span class="de-em">Uptime</span></h3>
-        <p>Rhoncus adipiscing, magna integer cursus augue eros lacus porttitor magna. Dictumst, odio! Elementum tortor sociis in eu dis dictumst pulvinar lorem nec aliquam a nascetur.</p>
+        <h3 class="title">DVR  AND STANDALONE</h3>
+        <spam>DVR 4/8/16/24/32 CHANNEL</spam><br>
+        <spam>NVR 4/8/16 CHANNEL</spam><br>
+        <spam>HVR 4/8/16 CHANNEL</spam>
       </div>
       <div class="feature col-sm-6 products-list"> <img src="img/features/HT-KB1010.png" alt="Feature 4" class="img-responsive" />
-        <h3 class="title">99.9% <span class="de-em">Uptime</span></h3>
-        <p>Rhoncus adipiscing, magna integer cursus augue eros lacus porttitor magna. Dictumst, odio! Elementum tortor sociis in eu dis dictumst pulvinar lorem nec aliquam a nascetur.</p>
+        <h3 class="title">Accessories</h3>
+        <spam>JOSTICK</spam><br>
+        <spam>VIDEO TESTER</spam><br>
+        <spam>POWER ADAPTER</spam><br>
+        <spam>FONT POWER</spam>
       </div>
     </div>
   </div>
